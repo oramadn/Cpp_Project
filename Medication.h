8a4991aa8ede67d7675d7384d645c25a0552661e
Toubasi1 <153913690+Toubasi1@users.noreply.github.com>
2023-12-23 20:27:01 +0300

    Update Medication.h

--- Medication.h
+++ Medication.h
@@ -1,33 +1,40 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#ifndef MEDICATION_H
+#define MEDICATION_H
 
-class Medication
-{
+#include "ExpiryDate.h"
+#include <iostream>
+
+class Medication {
 private:
-	string name_;
-	string description_;
-	string expiry_date_;
-	float price_;
-	int quantity_;
-	int barcode_;
+    string name_;
+    string description_;
+    float price_;
+    int quantity_;
+    ExpiryDate expiry_date_;
+    int barcode_;
 
 public:
-	Medication();
-	Medication(string name_, string description_, float price_, int quantity_, string expiry_date_, int barcode_);
-	Medication(const Medication& c);
-	void setName(std::string name_);
-	void setDescription(std::string description_);
-	void setPrice(float price_);
-	void setQuantity_in_Stock(int quantity_);
-	void setExpiry_Date(string expiry_date_);
-	void setbarcode(int barcode_);
-	std::string getName();
-	std::string getDescription();
-	double getPrice();
-	int getQuantity();
-	string getExpiry_Date();
-	int getBarcode();
-	void display_medication_information();
+    Medication(); // Default constructor
+    Medication(string name_, string description_, float price_, int quantity_, const ExpiryDate& expiry_date_, int barcode_); // Parameterized constructor
+    Medication(const Medication& c); // Copy constructor
+
+    // Setters
+    void setName(string name_);
+    void setDescription(string description_);
+    void setPrice(float price_);
+    void setQuantity_in_Stock(int quantity_);
+    void setExpiry_Date(const ExpiryDate& expiry_date_);
+    void setBarcode(int barcode_);
+
+    // Getters
+    string getName();
+    string getDescription();
+    float getPrice();
+    int getQuantity();
+    ExpiryDate getExpiry_Date();
+    int getBarcode();
+
+    void display_medication_information();
 };
 
+#endif // MEDICATION_H
